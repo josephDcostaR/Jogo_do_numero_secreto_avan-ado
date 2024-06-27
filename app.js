ff8f9ff8c4618d85832a0b7d8e2a74b46f296cc1
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2024-06-27 13:59:15 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,112 @@
+let listaDeNumerosSorteados = [];
+let dificuldade = 1;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Por favor, selecione um nível de dificuldade primeiro');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(numeroSecreto);
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function niveisDeDificuldade(event) {
+    let valor = event.target.dataset.value;
+    switch (valor) {
+        case 'facil':
+            dificuldade = 10;
+            exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+            break;
+        
+        case 'medio':
+            dificuldade = 100
+            exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+            break;
+
+        case 'dificil':
+            dificuldade =1000
+            exibirTextoNaTela('p', 'Escolha um número entre 1 e 1000');
+            break;
+    }
+
+    numeroSecreto = gerarNumeroAleatorio(); // Gera o número secreto após definir a dificuldade
+    tentativas = 1; // Reseta as tentativas
+    console.log(numeroSecreto);
+}
+
+let botoes = document.querySelectorAll('.container__botao__dificuldade');
+
+botoes.forEach(function(botao) {
+    botao.addEventListener('click', niveisDeDificuldade);
+});
+    
+
+function gerarNumeroAleatorio() {
+    if (dificuldade === 0) {
+        console.log("Por favor, selecione um nível de dificuldade primeiro.");
+        return;
+    }
+
+    let numeroEscolhido = parseInt(Math.random() * dificuldade + 1);
+
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == dificuldade) {
+      listaDeNumerosSorteados = [];
+    }
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+      return gerarNumeroAleatorio();
+    }else {
+      listaDeNumerosSorteados.push(numeroEscolhido);
+      console.log(listaDeNumerosSorteados)
+      return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+
+
+
+
+
+
+
